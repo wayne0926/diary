ba1a7b32db5537120cad0840c7c51cbd723ec0bf
wayne0926 <wayne0926@foxmail.com>
2020-10-10 20:23:33 +0800

    更新注释

--- diary.py
+++ diary.py
@@ -1,15 +1,26 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
+# 引入模块
 import time
-time1 = time.strftime("%Y年%m月%d日 %H:%M", time.localtime()) 
-# time = input('什么时间？')
+# 定义系统时间变量
+time = time.strftime("%Y年%m月%d日 %H:%M", time.localtime()) 
+# 询问名字
 name = input('鼎鼎大名：')
+# 询问天气
 weather = input('天气如何？')
+# 询问地点
 place = input('我在：')
+# 询问时间
 thing = input('做了啥事？')
+# 询问心情
 mood = input('心情如何？')
-out = '>>>> ' + time1 + '   ' + place + '   ' + weather + ' <<<<' + '\n'  + thing + '，' + '感觉' + mood + '\n' + '\n     >>>' + name + '<<<' + '\n'
-f = open('123.txt',mode='a')
+# 汇总所有内容
+out = '>>>> ' + time + '   ' + place + '   ' + weather + ' <<<<' + '\n'  + thing + '，' + '感觉' + mood + '\n' + '\n     >>>' + name + '<<<' + '\n'
+# 新建(打开)文件
+f = open('diary.txt',mode='a')
+# 写入(追加)文件
 f.write(out)
+# 关闭文件
 f.close
+# 打印结果
 print(out)
