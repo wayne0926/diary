1f1125868658f260fddb8517f3ccf251586a92d5
wayne0926 <wayne0926@foxmail.com>
2020-10-18 17:23:35 +0800

    测试自动获取

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-print("\033[1m完成：" + "已将文本输出在程序目录下的diary.txt")
+import requests
+playload = {'key': '91feef17aa2cd875a61f7520dd30207a'}
+pl = requests.get('https://restapi.amap.com/v3/ip', params=playload)
+re = pl.json()['adcode']
+print(re)
+playload1 = {'city': re, 'key': '91feef17aa2cd875a61f7520dd30207a', 'extensions': 'base'}
+we = requests.get('https://restapi.amap.com/v3/weather/weatherInfo', params=playload1)
+# print(we.json())
+rew = we.json()['lives'][0]['city']
+weas = we.json()['lives'][0]['weather']
+print(rew)
+print(weas)
