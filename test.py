faae025e465fabf437a4939c3012708a956ac035
wayne0926 <wayne0926@foxmail.com>
2020-10-22 19:48:55 +0800

    测试NTP

--- test.py
+++ test.py
@@ -1,12 +1,8 @@
-import requests
-playload = {'key': '91feef17aa2cd875a61f7520dd30207a'}
-pl = requests.get('https://restapi.amap.com/v3/ip', params=playload)
-re = pl.json()['adcode']
-print(re)
-playload1 = {'city': re, 'key': '91feef17aa2cd875a61f7520dd30207a', 'extensions': 'base'}
-we = requests.get('https://restapi.amap.com/v3/weather/weatherInfo', params=playload1)
-# print(we.json())
-rew = we.json()['lives'][0]['city']
-weas = we.json()['lives'][0]['weather']
-print(rew)
-print(weas)
+# -*- coding:utf-8 -*-
+
+import ntplib
+import datetime
+
+client = ntplib.NTPClient()
+response = client.request('ntp.aliyun.com')
+print(datetime.datetime.fromtimestamp(response.tx_time))
