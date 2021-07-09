5d5f17f7f6d6f59b44cb0a2d837d9feac3f3cef5
Wayne0926 <wayne0926@foxmail.com>
2021-07-09 23:44:31 +0800

    代码优化

--- diary.py
+++ diary.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
 # -*- coding: utf-8 -*-
 # 作者：魏然
-# 更新：2020/10/22
+# 更新：2021/7/9
 # 注意！：“丨”是中文里的“shù”
 # 引入模块
 import time
+import sys
+import os
+import datetime
 import requests
 import ntplib
-import datetime
 import codecs
-import os
 # 位置，天气，时间赋值
 # 设置高德API秘钥
 p = {'key': '91feef17aa2cd875a61f7520dd30207a'}
@@ -22,7 +23,8 @@ if t != '1':
 # 若“t”不是“1”（不成功）
 else:
     # GET获取API返回的JSON并取回“adcode”（高德位置编码），然后赋值给“pCode”
-    pCode = requests.get('https://restapi.amap.com/v3/ip', params=p).json()['adcode']
+    pCode = requests.get('https://restapi.amap.com/v3/ip',
+                         params=p).json()['adcode']
 # 判断是否成功（是否为“1”），若不成功（不等于“1”）
 if t != '1':
     # 手动输入信息（天气+位置）
@@ -33,11 +35,14 @@ else:
     # 设置高德API秘钥以及位置编码
     p = {'city': pCode, 'key': '91feef17aa2cd875a61f7520dd30207a', 'extensions': 'base'}
     # GET获取API返回的JSON并取回“city”（城市），然后赋值给“place”
-    place = requests.get('https://restapi.amap.com/v3/weather/weatherInfo', params=p).json()['lives'][0]['city']
+    place = requests.get(
+        'https://restapi.amap.com/v3/weather/weatherInfo', params=p).json()['lives'][0]['city']
     # GET获取API返回的JSON并取回“weather”（天气（中文提示）），然后赋值给“we”
-    we = requests.get('https://restapi.amap.com/v3/weather/weatherInfo', params=p).json()['lives'][0]['weather']
+    we = requests.get('https://restapi.amap.com/v3/weather/weatherInfo',
+                      params=p).json()['lives'][0]['weather']
     # GET获取API返回的JSON并取回“temperature”（温度），然后赋值给“tem”
-    tem = requests.get('https://restapi.amap.com/v3/weather/weatherInfo', params=p).json()['lives'][0]['temperature'] + '℃'
+    tem = requests.get('https://restapi.amap.com/v3/weather/weatherInfo',
+                       params=p).json()['lives'][0]['temperature'] + '℃'
     # 汇总天气（中文提示）与温度，并赋值给“wea”
     wea = we + '丨' + tem
 
@@ -59,7 +64,8 @@ thing = input('做了啥事？')
 # 询问心情
 mood = input('心情如何？')
 # 汇总所有内容
-out = '===== ' + time + '丨' + place + '丨' + wea + ' =====' + '\n' + thing + '，' + '感觉' + mood + '\n' + '\n    ===>' + name + '<===' + '\n\n'
+out = '===== ' + time + '丨' + place + '丨' + wea + ' =====' + '\n' + \
+    thing + '，' + '感觉' + mood + '\n' + '\n    ===>' + name + '<===' + '\n\n'
 # 将所有信息汇入表
 oul = [time, place, name, thing, wea, mood]
 # 依次打印(列表)
@@ -80,8 +86,6 @@ print('======================================================================')
 f = codecs.open('diary.txt', mode='a', encoding='utf-8')
 # 写入(追加)文件
 f.write(out)
-# 关闭文件
-f.close
 # 打印结果(输出)
 print(out)
 # 显示完成提示语
@@ -90,4 +94,4 @@ print("\033[1m完成：" + "已将文本输出在程序目录下的diary.txt")
 e = input('=============>回车保存退出<=============')
 # 判断并退出
 if e == '':
-    os._exit
+    sys.exit()
